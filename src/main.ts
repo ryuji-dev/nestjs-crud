4569113fb794426bb2b89baba7eda6a8a2fcb844
Ryuji <hi_ryuji@naver.com>
2025-03-06 13:09:26 +0900

    feat: 개발 환경 설정

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3001;
+  const config = new DocumentBuilder()
+    .setTitle('NestJS CRUD API')
+    .setDescription('NestJS CRUD API description')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  await app.listen(port);
+  console.log(`Server is running at http://localhost:${port}`);
+}
+void bootstrap();
